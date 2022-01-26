59c4e4f3ab995d283a1c132f20b84d53c171c0de
nok6868 <98205172+nok6868@users.noreply.github.com>
2022-01-26 22:42:57 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+const input = document.getElementById("data");
+const button = document.getElementById("btn");
+const qrImg = document.getElementById("img");
+
+function downloadImage(){
+    var input = document.getElementById("data").value;
+    var name = "QR-CODE "
+    var url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+input
+      fetch(url)
+        .then(resp => resp.blob())
+        .then(blob => {
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.style.display = 'none';
+            a.href = url;
+            // the filename you want
+            a.download = name;
+            document.body.appendChild(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+        })
+        .catch(() => alert('กรุณากรอกข้อมูลแล้วสร้าง QR-CODE ก่อน'));
+}
+
+button.addEventListener("click", ()=>{
+    let data = input.value;
+    if(data.length>0){
+        let qrsource="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+data;
+
+        qrImg.src=qrsource;
+    }
+})
